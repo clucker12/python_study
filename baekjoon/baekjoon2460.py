6607ba710fb2b164e3f510190525f597e2b9e8a3
seong ho <101807706+clucker12@users.noreply.github.com>
2022-05-20 16:15:24 +0900

    Create baekjoon2460.py

---
+++ baekjoon/baekjoon2460.py
@@ -0,0 +1,18 @@
+import sys
+
+# 탄사람 수 합 배열 선언
+hap = []
+# 합의 값 변수 선언
+s = 0
+# 10번 반복수행
+for i in range(10):
+    # 역마다 내린사람,탄사람수 배열 초기화
+    a = []
+    # 역마다 내린사람,탄사람수 배열에 대입
+    a = list(map(int, sys.stdin.readline().split()))
+    # 역마다 탄 사람수 합 구하기
+    s += (a[1] - a[0])
+    # 역마다 탄 사람수 합 배열에 넣기
+    hap.append(s)
+# 역마다 탄 사람수 합 최대값 구하기
+print(max(hap))
